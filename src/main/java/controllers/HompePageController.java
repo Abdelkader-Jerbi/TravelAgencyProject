f71975493d1ede1d13f867bc1a52a8d1e16f990b
arwa123-dridi <arwa.dridi@esprit.tn>
2025-05-24 21:22:58 +0200

    lienVol

--- src/main/java/controllers/HompePageController.java
+++ src/main/java/controllers/HompePageController.java
@@ -43,4 +43,15 @@ public class HompePageController {
             System.out.println(e.getMessage());
         }
     }
+    public void allerPageChercherVol(ActionEvent Event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/ChercherVol.fxml"));
+            Stage stage = (Stage) ((Node) Event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

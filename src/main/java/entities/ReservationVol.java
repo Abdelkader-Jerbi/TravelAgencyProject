64139a480ccaf709906214037fb5002153c885f7
Abdelkader-Jerbi <Abdelkader-Jerbi@esprit.tn>
2025-05-30 19:48:51 +0100

    reservationVol fix

--- src/main/java/entities/ReservationVol.java
+++ src/main/java/entities/ReservationVol.java
@@ -1,62 +1,50 @@
 package entities;
 
-import java.sql.Date;
-
+import java.util.Date;
 
 public class ReservationVol {
-
     private int idReservation;
+    private double prixTotal;
+    private String status;
     private Date dateReservation;
-    private Date dateDebut;
-    private Date dateFin;
-    private Float prixTotal;
-    private int nbPersonnes;
-    private String statut;
-    private Voiture voiture;
-    private String villeRetour;
-    private String villeDepart;
-    private String nom;
-    private String prenom;
+    private int idHotel;
+    private int idVol;
+    private int idUser;
+    private int idVoiture;
 
-
-    public ReservationVol(int idReservation, float prixTotal, String statut, Date dateReservation, int idHotel, int idVol, int idUser, int idVoiture) {
-
+    public ReservationVol(int idReservation, double prixTotal, String status, Date dateReservation, int idHotel, int idVol, int idUser, int idVoiture) {
         this.idReservation = idReservation;
-        this.dateReservation = dateReservation;
-        this.dateDebut = dateDebut;
-        this.dateFin = dateFin;
-        this.villeRetour = villeRetour;
-        this.villeDepart = villeDepart;
         this.prixTotal = prixTotal;
-        this.nbPersonnes = nbPersonnes;
-        this.statut = statut;
-        this.voiture = voiture;
-        this.nom = nom;
-        this.prenom = prenom;
+        this.status = status;
+        this.dateReservation = dateReservation;
+        this.idHotel = idHotel;
+        this.idVol = idVol;
+        this.idUser = idUser;
+        this.idVoiture = idVoiture;
     }
 
-    // ✅ (Optionnel) constructeur ReservationVol sans ID (utile avant insertion en base)
-    public ReservationVol(Date dateReservation, Date dateDebut, Date dateFin,
-                       String villeRetour, String villeDepart, Float prixTotal,
-                       int nbPersonnes, String statut, Voiture voiture) {
-        this.dateReservation = dateReservation;
-        this.dateDebut = dateDebut;
-        this.dateFin = dateFin;
-        this.villeRetour = villeRetour;
-        this.villeDepart = villeDepart;
-        this.prixTotal = prixTotal;
-        this.nbPersonnes = nbPersonnes;
-        this.statut = statut;
-        this.voiture = voiture;
-    }
-
-    // ✅ Getters & Setters
     public int getIdReservation() {
         return idReservation;
     }
 
     public void setIdReservation(int idReservation) {
         this.idReservation = idReservation;
+    }
+
+    public double getPrixTotal() {
+        return prixTotal;
+    }
+
+    public void setPrixTotal(double prixTotal) {
+        this.prixTotal = prixTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public Date getDateReservation() {
@@ -67,101 +55,35 @@ public class ReservationVol {
         this.dateReservation = dateReservation;
     }
 
-    public Date getDateDebut() {
-        return dateDebut;
+    public int getIdHotel() {
+        return idHotel;
     }
 
-    public void setDateDebut(Date dateDebut) {
-        this.dateDebut = dateDebut;
+    public void setIdHotel(int idHotel) {
+        this.idHotel = idHotel;
     }
 
-    public Date getDateFin() {
-        return dateFin;
+    public int getIdVol() {
+        return idVol;
     }
 
-    public void setDateFin(Date dateFin) {
-        this.dateFin = dateFin;
+    public void setIdVol(int idVol) {
+        this.idVol = idVol;
     }
 
-    public Float getPrixTotal() {
-        return prixTotal;
+    public int getIdUser() {
+        return idUser;
     }
 
-    public void setPrixTotal(Float prixTotal) {
-        this.prixTotal = prixTotal;
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
     }
 
-    public int getNbPersonnes() {
-        return nbPersonnes;
+    public int getIdVoiture() {
+        return idVoiture;
     }
 
-    public void setNbPersonnes(int nbPersonnes) {
-        this.nbPersonnes = nbPersonnes;
-    }
-
-    public String getStatut() {
-        return statut;
-    }
-
-    public void setStatut(String statut) {
-        this.statut = statut;
-    }
-
-    public Voiture getVoiture() {
-        return voiture;
-    }
-
-    public void setVoiture(Voiture voiture) {
-        this.voiture = voiture;
-    }
-
-    public String getVilleRetour() {
-        return villeRetour;
-    }
-
-    public void setVilleRetour(String villeRetour) {
-        this.villeRetour = villeRetour;
-    }
-
-    public String getVilleDepart() {
-        return villeDepart;
-    }
-
-    public void setVilleDepart(String villeDepart) {
-        this.villeDepart = villeDepart;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    @Override
-    public String toString() {
-        return "ReservationVol{" +
-                "idReservation=" + idReservation +
-                ", dateReservation=" + dateReservation +
-                ", dateDebut=" + dateDebut +
-                ", dateFin=" + dateFin +
-                ", prixTotal=" + prixTotal +
-                ", nbPersonnes=" + nbPersonnes +
-                ", statut='" + statut + '\'' +
-                ", voiture=" + voiture +
-                ", villeRetour='" + villeRetour + '\'' +
-                ", villeDepart='" + villeDepart + '\'' +
-                ", nom='" + nom + '\'' +
-                ", prenom='" + prenom + '\'' +
-                '}';
+    public void setIdVoiture(int idVoiture) {
+        this.idVoiture = idVoiture;
     }
 }
